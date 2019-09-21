3f690e777c086704198b890039df9bb624002d5e
Trevor Pennypacker <trevp@seas.upenn.edu>
2019-09-21 00:53:40 -0500

    got better board image, added solder func

--- main.py
+++ main.py
@@ -9,18 +9,18 @@ boardX = 39 # squares
 boardY = 63 # squares
 
 components = []
-components.append(Component('arduino', 0, 100, 0))
-components.append(Component('diode', 200, 100, 0))
-components.append(Component('led', 150, 50, 0))
-components.append(Component('SOIC8', 30, 50, 0))
+components.append(Component('arduino', 100, 100, 0))
+components.append(Component('diode', 50, 30, 0))
+components.append(Component('led', 250, 100, 0))
+components.append(Component('SOIC8', 20, 30, 0))
 
 
 # load the board image and scale it down
 board = cv2.imread('images/board.png', cv2.IMREAD_COLOR)
-height = int(63 * 3.1667)
-width = int(height * boardY / boardX)
-board = cv2.resize(board,(width, height))
 
+print("Board width: " + str(board.shape[1]) + ", board height: " + str(board.shape[0]))
+
+# iterate through the components and put them on the board
 for component in components:
     board = imageFuncs.insertComponent(board, component)
 
--- utils/imageFuncs.py
+++ utils/imageFuncs.py
@@ -26,15 +26,27 @@ def boundedRotation(image, angle):
 
 def insertImage(l_img, s_img, x, y, theta):
 
-	s_img = boundedRotation(s_img, theta)
-	l_img[y:y+s_img.shape[0], x:x+s_img.shape[1]] = s_img
+    s_img = boundedRotation(s_img, theta)
 
-	return l_img
+    halfY1 = int(s_img.shape[0] / 2)
+    halfX1 = int(s_img.shape[1] / 2)
+    halfY2 = s_img.shape[0] - halfY1
+    halfX2 = s_img.shape[1] - halfX1
+
+    l_img[y-halfY1:y+halfY2, x-halfX1:x+halfX2] = s_img
+
+    return l_img
 
 
 def insertComponent(board, component):
     
     print("Inserting " + component.name + " at  (" + str(component.x) + ", " + str(component.y) + ")")
     componentImg = cv2.imread('images/' + component.name + '.png')
+    print(componentImg[0, 0])
+
     print("Width: " + str(componentImg.shape[1]) + ", Height: " + str(componentImg.shape[0]))
     return insertImage(board, componentImg, component.x, component.y, component.theta)
+
+
+def drawSolder(board, x0, y0, x1, y1):
+    return cv2.line(board, (x0, y0), (x1, y1), (50,50,50), 1)
